c2b3a1a42852fa9167d97aecc6d7cdbd8cba5651
sadufo <maxzaplitnyy@gmail.com>
2022-06-19 17:05:38 +0300

    tablet

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded',e=>{
+    const animItem = document.querySelectorAll('.anim_item')
+
+animItem.forEach(el=>{
+    document.addEventListener('scroll',e=>{
+       if(el.getBoundingClientRect().y<=300){
+           el.classList.remove('left')
+           el.classList.remove('right')
+           el.classList.remove('top')
+           el.classList.remove('bottom')
+           setTimeout(() => {
+            el.classList.remove('anim_item')
+           }, 1000);
+       }
+    }) 
+})
 
     const pre = document.querySelector('.wrapper_preloader')
     document.body.style.height = '100vh'
